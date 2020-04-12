cc50a80b510ad6e472e14c0ba18cde7254b49d33
Mark Cascardi <mcascardi@gmail.com>
2020-04-12 00:40:05 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -1,4 +1,31 @@
-//define functions here
+function getIt() {
+  $('p').on('click', function() {
+    alert("Hey!")
+  }
+)}
+
+function frameIt() {
+  $('img').on('load', function() {
+    $('.tasty').appendTo('img')
+  }
+)}
+
+function pressIt() {
+  $('#typing').on('keydown', function(key) {
+    if(key.which == 71){
+      alert('g was pressed')
+    }
+  }
+)}
+
+function submitIt() {
+  $('form').on('submit', function() {
+    // if ($( "input:first" ).val() === "correct"){
+      alert('Your form is going to be submitted now."')
+    // }
+  }
+)}
+
 
 $(document).ready(function(){
 
